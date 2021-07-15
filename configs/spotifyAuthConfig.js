58cd783eb2b49990e52bf6bf5ef0c6b11e279561
Shaya94 <yginzburg94@gmail.com>
2021-07-15 13:49:04 +0300

    Added css file to start page

--- configs/spotifyAuthConfig.js
+++ configs/spotifyAuthConfig.js
@@ -1,14 +1,14 @@
 module.exports.spotifyAuthConfig = () => {
-    const baseAuthUrl = 'https://accounts.spotify.com/authorize';
-    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const baseAuthUrl = "https://accounts.spotify.com/authorize";
+    const tokenUrl = "https://accounts.spotify.com/api/token";
     const scopes = [
-        'app-remote-control',
-        'streaming',
-        'playlist-modify-public',
-        'playlist-modify-private',
-        'playlist-read-private',
-        'playlist-read-collaborative',
-        'user-read-currently-playing',
+        "app-remote-control",
+        "streaming",
+        "playlist-modify-public",
+        "playlist-modify-private",
+        "playlist-read-private",
+        "playlist-read-collaborative",
+        "user-read-currently-playing",
     ];
 
     const clientId = process.env.CLIENT_ID;
@@ -16,11 +16,11 @@ module.exports.spotifyAuthConfig = () => {
     const redirectUri = `http://localhost:3000/now`;
 
     const authorizationHeaderString =
-        'Basic ' +
-        Buffer.from(clientId + ':' + clientSecret).toString('base64');
+        "Basic " +
+        Buffer.from(clientId + ":" + clientSecret).toString("base64");
 
     const authUrl = `${baseAuthUrl}?response_type=code&client_id=${clientId}&scope=${scopes.join(
-        '%20'
+        "%20"
     )}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
     return {
--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
-const User = require('../models/user');
-const axios = require('axios');
-const express = require('express');
-const session = require('express-session');
-const ejsMate = require('ejs-mate');
-const { spotifyAuthConfig } = require('../configs/spotifyAuthConfig');
-const { getToken, getDetails, searchTracks } = require('../helpers');
+// const mongoose = require("mongoose");
+const User = require("../models/user");
+const axios = require("axios");
+const express = require("express");
+const session = require("express-session");
+const ejsMate = require("ejs-mate");
+const { spotifyAuthConfig } = require("../configs/spotifyAuthConfig");
+const { getToken, getDetails, searchTracks } = require("../helpers");
 spotifyAuth = spotifyAuthConfig();
 const router = express.Router();
-const { validateTokenExpiration } = require('../middleware');
+const { validateTokenExpiration } = require("../middleware");
 
 // router.use('/', async (req, res, next) => {
 //     // if ()
@@ -17,24 +17,24 @@ const { validateTokenExpiration } = require('../middleware');
 //     next();
 // });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
     res.redirect(spotifyAuth.authUrl);
 });
 
-router.post('/search', async (req, res) => {
+router.post("/search", async (req, res) => {
     const user = await User.findOne({ spotify_id: req.body.id });
     if (!req.body.q) return;
     const searchResults = await searchTracks(user, req.body);
     res.send(searchResults);
 });
 
-router.get('/now', async (req, res) => {
+router.get("/now", async (req, res) => {
     //send post request to Spotify
     const response = await getToken(req.query.code, spotifyAuth);
     token = response.data;
 
     if (response.status !== 200) {
-        res.send('error');
+        res.send("error");
     }
     const userDetails = await getDetails(token);
     // console.log(token);
@@ -46,7 +46,9 @@ router.get('/now', async (req, res) => {
             {
                 username: userDetails.data.display_name,
                 spotify_id: userDetails.data.id,
-                image_url: userDetails.data.images[0].url,
+                image_url: userDetails.data.images[0].url
+                    ? userDetails.data.images[0].url
+                    : null,
                 token: {
                     access_token: token.access_token,
                     token_type: token.token_type,
@@ -74,11 +76,11 @@ router.get('/now', async (req, res) => {
     }
     // console.log(user);
 
-    res.cookie('SpotifyAccess', user.spotify_id);
-    res.render('loggedin', { user: userDetails.data });
+    res.cookie("SpotifyAccess", user.spotify_id);
+    res.render("loggedin", { user: userDetails.data });
 });
-router.get('/', (req, res) => {
-    res.render('start');
+router.get("/", (req, res) => {
+    res.render("start");
 });
 
 module.exports = router;
