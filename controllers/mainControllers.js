e3222b661e6217be54250e2afd46ecf550e6ba89
Ziv Kaplan <zivkaplan@gmail.com>
2021-07-20 13:30:10 +0300

    added comment

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -96,6 +96,7 @@ module.exports.loggedInPage = async (req, res) => {
     try {
         let user;
         if (!req.session.firstLogin) {
+            //check if the user logegd in and redirected from spotify
             if (!req.query.code) {
                 return res.render('start');
             }
