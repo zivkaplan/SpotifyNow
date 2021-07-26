b43fb5d797ae6665457d9b0bd440410de79cf857
Ziv Kaplan <zivkaplan@gmail.com>
2021-07-26 14:20:09 +0300

    secure cookie

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const appConfig = (function () {
             expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
             maxAge: 1000 * 60 * 60 * 24 * 7,
             httpOnly: true,
-            // secure: true,
+            secure: true,
         },
         store: MongoStore.create({
             mongoUrl: dbUrl,
